bf6a7403c888e76e396d1ff980547e765a978bf8
moritz <moritzaznan@gmail.com>
2017-02-22 17:59:09 +0100

    content for edit alarm

--- app/routes/alarms/edit.js
+++ app/routes/alarms/edit.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+    model(params){
+        return{
+             id: 1,
+             time:"17:00",
+             title: "Erinnerung",
+             on: true,
+             repeated: false,
+            };
+    },
+
+    actions:{
+        save(model){
+            console.debug(model);
+            this.transitionTo('index');
+        }
+    }
 });
