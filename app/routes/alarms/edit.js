4bff2936948f96a3ee76cf8b4e2b7c1154ab64e9
moritz <moritzaznan@gmail.com>
2017-02-23 17:25:10 +0100

    save new alarm

--- app/routes/alarms/edit.js
+++ app/routes/alarms/edit.js
@@ -2,15 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-    model(params){
-        return{
-             id: 1,
-             time:"17:00",
-             title: "Erinnerung",
-             on: true,
-             repeated: false,
-            };
-    },
+    
 
     actions:{
         save(model){
