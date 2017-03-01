0bd80a85f0dbcf9e4ea60e667a4f5ab954d4c003
moritz <moritzaznan@gmail.com>
2017-03-01 20:24:58 +0100

    update edit for local storage

--- app/routes/alarms/edit.js
+++ app/routes/alarms/edit.js
@@ -2,12 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-    
+    model(params) {
+        return this.store.findRecord('alarm', params.id);
+    },
+
+    // Generic Rollback for dirty models
+    deactivate(){
+        let model = this.get('currentModel');
+        if (model.get('hasDirtyAttributes')){
+            model.rollbackAttributes();
+        }
+    },
 
     actions:{
-        save(model){
-            console.debug(model);
-            this.transitionTo('index');
+        save(){
+            let model = this.get('currentModel');
+            model.save().then(() => this.transitionTo('index'));
+            
         },
 
         cancel(model){
