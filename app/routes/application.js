1a1dc697693f224f17217b0b65e2a3738850d088
moritz <moritzaznan@gmail.com>
2017-03-01 22:21:06 +0100

    destroy Alarm

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 
 
 export default Ember.Route.extend({
-   
+   actions:{
+     deleteAlarm(alarm){
+         alarm.destroyRecord();
+         this.transitionTo('index');
+     }  
+   }
 });
