1f7e50736fd22f31dc9f08b6402b8a086dc5cb36
moritz <moritzaznan@gmail.com>
2017-03-03 12:38:40 +0100

    Adding OnOff toggle in index

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,17 @@ import Ember from 'ember';
 export default Ember.Route.extend({
      model(){
          return this.store.findAll('alarm');
+     },
+
+     actions:{
+         toggleOnOff(alarm){
+            if(alarm.get('on')){
+                alarm.set('on', false);
+            }
+            else{
+                alarm.set('on', true);
+            };
+            alarm.save();
+        }
      }
 });
